067a106735f496ad0ffdd7e4f971da7616fa0729
Nikhil Vas <nikhilvas123@gmail.com>
2020-01-16 08:54:17 +0530

    Basic Framework

---
+++ Slither.py
@@ -0,0 +1,13 @@
+import pygame
+
+#Initialize
+pygame.init()
+
+#Canvas
+gameDisplay = pygame.display.set_mode((800,600))
+
+#pygame.display.flip() Whole surface update
+pygame.display.update()
+
+pygame.quit()
+quit()
