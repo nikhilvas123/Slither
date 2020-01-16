d5d87d04016a945373c9de10418b8edd75081905
Nikhil Vas <nikhilvas123@gmail.com>
2020-01-16 23:36:03 +0530

    Moving Up

--- Slither.py
+++ Slither.py
@@ -14,6 +14,8 @@ gameExit = False
 lead_x = 300
 lead_y = 300
 lead_x_change = 0
+lead_y_change = 0
+
 clock = pygame.time.Clock()
 
 while not gameExit:
@@ -24,8 +26,16 @@ while not gameExit:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
 				lead_x_change = -10
-			if event.key == pygame.K_RIGHT:
+				lead_y_change = 0
+			elif event.key == pygame.K_RIGHT:
 				lead_x_change = 10
+				lead_y_change = 0
+			elif event.key == pygame.K_UP:
+				lead_y_change = -10
+				lead_x_change = 0
+			elif event.key == pygame.K_DOWN:
+				lead_y_change = 10
+				lead_x_change = 0
 
 	# To Stop Moving the rectangle
 	#	if event.type == pygame.KEYUP:
@@ -33,6 +43,7 @@ while not gameExit:
 	#			lead_x_change = 0
 
 	lead_x+= lead_x_change
+	lead_y+= lead_y_change
 
 	gameDisplay.fill(white)
 	pygame.draw.rect(gameDisplay,black,[lead_x,lead_y,10,10])
