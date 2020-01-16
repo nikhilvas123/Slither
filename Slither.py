326e2108a5f2fa6554b4143691117c3013f0b825
Nikhil Vas <nikhilvas123@gmail.com>
2020-01-16 23:20:59 +0530

    Frames Per Second

--- Slither.py
+++ Slither.py
@@ -14,6 +14,7 @@ gameExit = False
 lead_x = 300
 lead_y = 300
 lead_x_change = 0
+clock = pygame.time.Clock()
 
 while not gameExit:
 	for event in pygame.event.get():
@@ -30,9 +31,9 @@ while not gameExit:
 
 	gameDisplay.fill(white)
 	pygame.draw.rect(gameDisplay,black,[lead_x,lead_y,10,10])
-
 	pygame.display.update()
 
+	clock.tick(15)
 
 pygame.quit()
 quit()
